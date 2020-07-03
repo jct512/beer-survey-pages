64d92f36b15e11615eb33b03eaa9b92b428acac7
HangSit <jct512@alumni.ku.dk>
2020-07-03 21:47:58 +0200

    delete 8,9

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ window.onload = function() {
 }
 
 setTimeout(function(){
-   var arr=['https://www.survey-xact.dk/LinkCollector?key=EAGFKW21LP3N','https://www.survey-xact.dk/LinkCollector?key=EELETAD2S29N','https://www.survey-xact.dk/LinkCollector?key=M2G9TXD2J216','https://www.survey-xact.dk/LinkCollector?key=QWEETF4NUJ11','https://www.survey-xact.dk/LinkCollector?key=N65WMXDKLN1K','https://www.survey-xact.dk/LinkCollector?key=AN7ST9AJJJ9N','https://www.survey-xact.dk/LinkCollector?key=85EETEAKLN91','https://www.survey-xact.dk/LinkCollector?key=AAGEVS22J23J','https://www.survey-xact.dk/LinkCollector?key=KE7EK9A6UK9J','https://www.survey-xact.dk/LinkCollector?key=6E79MXA6S232'];
+   var arr=['https://www.survey-xact.dk/LinkCollector?key=EAGFKW21LP3N','https://www.survey-xact.dk/LinkCollector?key=EELETAD2S29N','https://www.survey-xact.dk/LinkCollector?key=M2G9TXD2J216','https://www.survey-xact.dk/LinkCollector?key=QWEETF4NUJ11','https://www.survey-xact.dk/LinkCollector?key=N65WMXDKLN1K','https://www.survey-xact.dk/LinkCollector?key=AN7ST9AJJJ9N','https://www.survey-xact.dk/LinkCollector?key=85EETEAKLN91','https://www.survey-xact.dk/LinkCollector?key=6E79MXA6S232'];
   window.location.href=arr[parseInt(Math.random()*arr.length)];
   },100);
