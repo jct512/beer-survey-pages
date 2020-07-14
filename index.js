4e5c6d51ba10eca5953ec35ad9f1ad938b9fbd33
Hang Sit <54843074+jct512@users.noreply.github.com>
2020-07-14 11:52:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ window.onload = function() {
 }
 
 setTimeout(function(){
-   var arr=['https://www.survey-xact.dk/LinkCollector?key=6E79MXA6S232','https://www.survey-xact.dk/LinkCollector?key=5RNGVS45LN3P','https://www.survey-xact.dk/LinkCollector?key=Y3QDMXDNJ63J','https://www.survey-xact.dk/LinkCollector?key=DRNCMW41S136','https://www.survey-xact.dk/LinkCollector?key=KRQZTT2JU216','https://www.survey-xact.dk/LinkCollector?key=FRYDMTANSP1N','https://www.survey-xact.dk/LinkCollector?key=CKDVVFA1SJC2','https://www.survey-xact.dk/LinkCollector?key=YP4UVS4PL5CK','https://www.survey-xact.dk/LinkCollector?key=KJDYKXDKSP1J'];
+   var arr=['https://www.survey-xact.dk/LinkCollector?key=6E79MXA6S232','https://www.survey-xact.dk/LinkCollector?key=5RNGVS45LN3P','https://www.survey-xact.dk/LinkCollector?key=Y3QDMXDNJ63J','https://www.survey-xact.dk/LinkCollector?key=DRNCMW41S136','https://www.survey-xact.dk/LinkCollector?key=KRQZTT2JU216','https://www.survey-xact.dk/LinkCollector?key=FRYDMTANSP1N','https://www.survey-xact.dk/LinkCollector?key=CKDVVFA1SJC2','https://www.survey-xact.dk/LinkCollector?key=YP4UVS4PL5CK','https://www.survey-xact.dk/LinkCollector?key=KJDYKXDKSP1J','https://www.survey-xact.dk/LinkCollector?key=3ME46TD2J115'];
   window.location.href=arr[parseInt(Math.random()*arr.length)];
   },100);
